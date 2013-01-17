7c05a0fa83bdc954ad83bde8b125136b24386780
Peter Suggitt <me@suggs.org.uk>
2013-01-17 09:03:50 +0000

    Gotten rid of gradle

---
+++ sockets/src/main/java/org/suggs/networking/sockets/GetSomethingThroughSocket.java
@@ -0,0 +1,8 @@
+package org.suggs.networking.sockets;
+
+/**
+ * TODO: Justify why you have written this class!
+ * To change this template use File | Settings | File Templates.
+ */
+public class GetSomethingThroughSocket {
+}
