7c50fd3cf530ab818928a7bddd059ad0596fa885
Peter Suggitt <me@suggs.org.uk>
2013-01-17 19:54:47 +0000

    Removed old java files

--- sockets/src/main/java/org/suggs/networking/sockets/ServerAllowingGetSomething.java
+++
@@ -1,8 +0,0 @@
-package org.suggs.networking.sockets;
-
-/**
- * TODO: Justify why you have written this class!
- * To change this template use File | Settings | File Templates.
- */
-public class ServerAllowingGetSomething {
-}
